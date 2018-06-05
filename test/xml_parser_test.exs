ba9e79f0bd4c88ed236f3242bd60b8c09aa56bd7
Milen Donchev <milen_d@abv.bg>
2018-06-06 00:16:47 +0300

    Add more complex xpath tests

--- test/xml_parser_test.exs
+++ test/xml_parser_test.exs
@@ -2,6 +2,9 @@ defmodule XmlParserTest do
   use ExUnit.Case
   doctest XmlParser
 
+  # TODO: add local-name() and namespaces
+  # TODO: add more functions from xpath
+
   test "can extract simple node with xpath" do
     xml = "<person ref=\"1234\"><name title=\"\">pesho</name></person>"
 
@@ -19,9 +22,82 @@ defmodule XmlParserTest do
   end
 
   test "can extract complex nodes with xpath" do
-    xml = "<list><item>one</item><item>two</item></list>"
+    xml = """
+    <?xml version="1.0" encoding="UTF-8"?>
+    <bookstore>
+        <book category="cooking">
+            <title lang="en">Everyday Italian</title>
+            <author>Giada De Laurentiis</author>
+            <year>2005</year>
+            <price>30.00</price>
+        </book>
 
-    # extract
-    assert ["one", "two"] == XmlParser.parse_string(xml) |> XmlParser.get("//item")
+        <book category="web">
+            <title lang="bg">XQuery Kick Start</title>
+            <author>James McGovern</author>
+            <author>Per Bothner</author>
+            <author>Kurt Cagle</author>
+            <author>James Linn</author>
+            <author>Vaidyanathan Nagarajan</author>
+            <year>2003</year>
+            <price>49.99</price>
+        </book>
+
+        <book empty="yes">
+        </book>
+
+        <book category="web">
+            <title lang="en">Learning XML</title>
+            <author>Erik T. Ray</author>
+            <year>2003</year>
+            <price>39.95</price>
+        </book>
+    </bookstore>
+    """
+
+    # selects the first book element
+    assert "Everyday Italian" ==
+             XmlParser.parse_string(xml)
+             |> XmlParser.get("/bookstore/book[1]/title")
+
+    # selects the last book element
+    assert "Learning XML" ==
+             XmlParser.parse_string(xml)
+             |> XmlParser.get("/bookstore/book[last()]/title")
+
+    # selects the (last - 2) book element
+    assert "XQuery Kick Start" ==
+             XmlParser.parse_string(xml)
+             |> XmlParser.get("/bookstore/book[last()-2]/title")
+
+    # selects all book elements with title element after the first book element
+    assert ["XQuery Kick Start", "Learning XML"] ==
+             XmlParser.parse_string(xml)
+             |> XmlParser.get("/bookstore/book[position()>1]/title")
+
+    # TODO: selects the first book element with title element
+    # assert "Everyday Italian" ==
+    #          XmlParser.parse_string(xml)
+    #          |> XmlParser.get("/bookstore/book[position()<2]/title")
+
+    # selects all the attributes of title elements that have an attribute named lang
+    assert ["en", "bg", "en"] ==
+             XmlParser.parse_string(xml)
+             |> XmlParser.get("//title/@lang")
+
+    # selects all the title elements that have an attribute named lang
+    assert ["Everyday Italian", "XQuery Kick Start", "Learning XML"] ==
+             XmlParser.parse_string(xml)
+             |> XmlParser.get("//title[@lang]")
+
+    # selects all the title elements that have a "lang" attribute equals "en"
+    assert ["Everyday Italian", "Learning XML"] ==
+             XmlParser.parse_string(xml)
+             |> XmlParser.get("//title[@lang='en']")
+
+    # TODO: selects all the title elements of the books that have a price greater than 35.00
+    # assert ["XQuery Kick Start", "Learning XML"] ==
+    #          XmlParser.parse_string(xml)
+    #          |> XmlParser.get("/bookstore/book[price>35.00]/title")
   end
 end
