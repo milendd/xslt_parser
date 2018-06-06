e25b5f83d5c3e064b65fc311a47762be1b0e0c71
milendd <milen_d@abv.bg>
2018-06-06 15:58:25 +0300

    Organize test structure and add more tests

--- test/xml_parser_test.exs
+++ test/xml_parser_test.exs
@@ -4,100 +4,118 @@ defmodule XmlParserTest do
 
   # TODO: add local-name() and namespaces
   # TODO: add more functions from xpath
+  # TODO: add wildcards
 
   test "can extract simple node with xpath" do
-    xml = "<person ref=\"1234\"><name title=\"\">pesho</name></person>"
+    simple_xml = "<person ref=\"1234\"><name title=\"\">pesho</name></person>"
 
     # extract text from element
-    assert "pesho" == XmlParser.parse_string(xml) |> XmlParser.get("//person/name")
+    assert "pesho" == XmlParser.parse_string(simple_xml) |> XmlParser.get("//person/name")
 
     # extract text from missing element
-    assert nil == XmlParser.parse_string(xml) |> XmlParser.get("//no/path")
+    assert nil == XmlParser.parse_string(simple_xml) |> XmlParser.get("//no/path")
 
     # extract text from root attribute
-    assert "1234" == XmlParser.parse_string(xml) |> XmlParser.get("//person/@ref")
+    assert "1234" == XmlParser.parse_string(simple_xml) |> XmlParser.get("//person/@ref")
 
     # extract text from attribute
-    assert "" == XmlParser.parse_string(xml) |> XmlParser.get("//name/@title")
+    assert "" == XmlParser.parse_string(simple_xml) |> XmlParser.get("//name/@title")
   end
 
   test "can extract complex nodes with xpath" do
-    xml = """
-    <?xml version="1.0" encoding="UTF-8"?>
-    <bookstore>
-        <book category="cooking">
-            <title lang="en">Everyday Italian</title>
-            <author>Giada De Laurentiis</author>
-            <year>2005</year>
-            <price>30.00</price>
-        </book>
-
-        <book category="web">
-            <title lang="bg">XQuery Kick Start</title>
-            <author>James McGovern</author>
-            <author>Per Bothner</author>
-            <author>Kurt Cagle</author>
-            <author>James Linn</author>
-            <author>Vaidyanathan Nagarajan</author>
-            <year>2003</year>
-            <price>49.99</price>
-        </book>
-
-        <book empty="yes">
-        </book>
-
-        <book category="web">
-            <title lang="en">Learning XML</title>
-            <author>Erik T. Ray</author>
-            <year>2003</year>
-            <price>39.95</price>
-        </book>
-    </bookstore>
-    """
+    doc = XmlTestContainer.get_books_xml() |> XmlParser.parse_string()
 
     # selects the first book element
-    assert "Everyday Italian" ==
-             XmlParser.parse_string(xml)
-             |> XmlParser.get("/bookstore/book[1]/title")
+    result = XmlParser.get(doc, "/bookstore/book[1]/title")
+    assert "Everyday Italian" == result
 
     # selects the last book element
-    assert "Learning XML" ==
-             XmlParser.parse_string(xml)
-             |> XmlParser.get("/bookstore/book[last()]/title")
+    result = XmlParser.get(doc, "/bookstore/book[last()]/title")
+    assert "Learning XML" == result
 
     # selects the (last - 2) book element
-    assert "XQuery Kick Start" ==
-             XmlParser.parse_string(xml)
-             |> XmlParser.get("/bookstore/book[last()-2]/title")
-
-    # selects all book elements with title element after the first book element
-    assert ["XQuery Kick Start", "Learning XML"] ==
-             XmlParser.parse_string(xml)
-             |> XmlParser.get("/bookstore/book[position()>1]/title")
-
-    # TODO: selects the first book element with title element
-    # assert "Everyday Italian" ==
-    #          XmlParser.parse_string(xml)
-    #          |> XmlParser.get("/bookstore/book[position()<2]/title")
+    result = XmlParser.get(doc, "/bookstore/book[last()-2]/title")
+    assert "XQuery Kick Start" == result
 
     # selects all the attributes of title elements that have an attribute named lang
-    assert ["en", "bg", "en"] ==
-             XmlParser.parse_string(xml)
-             |> XmlParser.get("//title/@lang")
+    result = XmlParser.get(doc, "//title/@lang")
+    assert ["en", "bg", "en"] == result
 
     # selects all the title elements that have an attribute named lang
-    assert ["Everyday Italian", "XQuery Kick Start", "Learning XML"] ==
-             XmlParser.parse_string(xml)
-             |> XmlParser.get("//title[@lang]")
+    result = XmlParser.get(doc, "//title[@lang]")
+    assert ["Everyday Italian", "XQuery Kick Start", "Learning XML"] == result
 
     # selects all the title elements that have a "lang" attribute equals "en"
-    assert ["Everyday Italian", "Learning XML"] ==
-             XmlParser.parse_string(xml)
-             |> XmlParser.get("//title[@lang='en']")
+    result = XmlParser.get(doc, "//title[@lang='en']")
+    assert ["Everyday Italian", "Learning XML"] == result
 
     # TODO: selects all the title elements of the books that have a price greater than 35.00
-    # assert ["XQuery Kick Start", "Learning XML"] ==
-    #          XmlParser.parse_string(xml)
-    #          |> XmlParser.get("/bookstore/book[price>35.00]/title")
+    # result = XmlParser.get(doc, "/bookstore/book[price>35.00]/title")
+    # assert ["XQuery Kick Start", "Learning XML"] == result
+  end
+
+  test "can use position() with '=' operator in xpath" do
+    doc = XmlTestContainer.get_books_xml() |> XmlParser.parse_string()
+
+    # selects nil from book elements, because position starts from 1
+    result = XmlParser.get(doc, "/bookstore/book[position()=0]")
+    assert nil == result
+
+    result = XmlParser.get(doc, "/bookstore/book[position()=1]/title")
+    assert "Everyday Italian" == result
+
+    result = XmlParser.get(doc, "/bookstore/book[position()=2]/title")
+    assert "XQuery Kick Start" == result
+
+    result = XmlParser.get(doc, "/bookstore/book[position()=3]/title")
+    assert nil == result
+
+    result = XmlParser.get(doc, "/bookstore/book[position()=4]/title")
+    assert "Learning XML" == result
+
+    result = XmlParser.get(doc, "/bookstore/book[position()=last()]/title")
+    assert "Learning XML" == result
+
+    result = XmlParser.get(doc, "/bookstore/book[position()=last()-2]/title")
+    assert "XQuery Kick Start" == result
+  end
+
+  test "can use position() with '>' greater operator" do
+    doc = XmlTestContainer.get_books_xml() |> XmlParser.parse_string()
+
+    result = XmlParser.get(doc, "/bookstore/book[position()>0]/title")
+    assert ["Everyday Italian", "XQuery Kick Start", "Learning XML"] == result
+
+    result = XmlParser.get(doc, "/bookstore/book[position()>1]/title")
+    assert ["XQuery Kick Start", "Learning XML"] == result
+
+    result = XmlParser.get(doc, "/bookstore/book[position()>2]/title")
+    assert "Learning XML" == result
+
+    result = XmlParser.get(doc, "/bookstore/book[position()>3]/title")
+    assert "Learning XML" == result
+
+    result = XmlParser.get(doc, "/bookstore/book[position()>4]/title")
+    assert nil == result
+  end
+
+  test "can use position() with '<' less operator" do
+    # doc = XmlTestContainer.get_books_xml() |> XmlParser.parse_string()
+
+    # TODO:
+    # result = XmlParser.get(doc, "/bookstore/book[position()<1]/title")
+    # assert nil == result
+
+    # TODO:
+    # result = XmlParser.get(doc, "/bookstore/book[position()<2]/title")
+    # assert "Everyday Italian" == result
+
+    # TODO:
+    # result = XmlParser.get(doc, "/bookstore/book[position()<3]/title")
+    # assert ["Everyday Italian", "XQuery Kick Start"] == result
+
+    # TODO:
+    # result = XmlParser.get(doc, "/bookstore/book[position()<5]/title")
+    # assert ["Everyday Italian", "XQuery Kick Start", "Learning XML"] == result
   end
 end
