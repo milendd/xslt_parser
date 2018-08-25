a14f9ec443c05f58d534132bf49aef77e58007ce
Milen Donchev <milen_d@abv.bg>
2018-08-25 10:43:54 +0300

    Convert from record to xml
    For now supports only elements and text

--- lib/xml_constructor.ex
+++ lib/xml_constructor.ex
@@ -3,9 +3,55 @@ defmodule XmlConstructor do
   Documentation for XmlConstructor.
   """
 
-  def parse_record(_record) do
-    # TODO: construct structure for XmlBuilder to use
-    # XmlBuilder.doc()
-    nil
+  require Record
+  require RecordHelper
+
+  def parse_record(element) do
+    data = [nil, %{}, nil]
+
+    case elem(element, 0) do
+      :xmlElement -> handle_element(data, element) |> List.to_tuple
+      :xmlText -> handle_text(data, element)
+      :xmlAttribute -> List.to_tuple(data) # IO.inspect("io: attr") # handle_attribute(node)
+      _ -> List.to_tuple(data) # handle_the_others(node)
+    end
   end
+  
+  defp handle_element(data, element) do
+    fn_walk_children = fn (elm) ->
+      parse_record(elm)
+    end
+
+    children = RecordHelper.xmlElement(element, :content)
+    child_results = Enum.map(children, fn_walk_children) |> Enum.filter(& !is_nil(&1))
+
+    name = RecordHelper.xmlElement(element, :name)
+    data |> List.replace_at(0, name) |> List.replace_at(2, child_results)
+  end
+
+  defp handle_text(data, element) do
+    text = get_text(element) 
+      |> String.replace("\r", "")
+      |> String.replace("\t", "")
+      |> String.replace("\n", "")
+    
+    case text do
+      "" -> nil
+      _ -> text
+    end
+  end
+
+  defp get_text(element) do
+    RecordHelper.xmlText(element, :value) |> List.to_string
+  end
+
+  # TODO: remove
+  # defp get_name(element) do
+  #   case elem(element, 0) do
+  #     :xmlElement -> RecordHelper.xmlElement(element, :name)
+  #     :xmlText -> RecordHelper.xmlText(element, :value) |> List.to_string
+  #     :xmlAttribute -> nil # RecordHelper.xmlAttribute(element, :value) |> List.to_string
+  #     _ -> nil
+  #   end
+  # end
 end
--- test/xml_constructor_test.exs
+++ test/xml_constructor_test.exs
@@ -1,5 +1,6 @@
 defmodule XmlConstructorTest do
   use ExUnit.Case
+  require RecordHelper
 
   doctest XmlConstructor
 
@@ -14,5 +15,28 @@ defmodule XmlConstructorTest do
     assert person_nil == {:person, %{}, nil} |> XmlBuilder.generate()
     assert person_nil == {:person, nil, nil} |> XmlBuilder.generate()
     assert person_nil == {:person, nil, nil} |> XmlBuilder.generate()
+
+    list = [:person, %{}, nil]
+    assert person_nil == List.to_tuple(list) |> XmlBuilder.generate()
+    assert person_empty_doc == person_nil |> XmlBuilder.doc()
+    
+    person_full = "<person>test</person>"
+    assert person_full == {:person, %{}, List.to_string('test')} |> XmlBuilder.generate()
+  end
+
+  test "construct simple tuples for xml-s" do
+    {:ok, xml} = File.read("test/samples/bib.xml")
+    doc_xml = xml |> XmlParser.parse_string()
+
+    # TODO:
+    # IO.inspect(doc_xslt)
+    # is_record = Record.is_record(doc_xslt, :xmlElement)
+    # IO.puts(is_record);
+
+    # result = XmlConstructor.parse_record(doc_xml) |> XmlBuilder.generate() |> XmlBuilder.doc()
+    parsedRecord = XmlConstructor.parse_record(doc_xml)
+    IO.inspect(parsedRecord)
+    result = parsedRecord |> XmlBuilder.generate() |> XmlBuilder.doc()
+    # IO.inspect(result)
   end
 end
